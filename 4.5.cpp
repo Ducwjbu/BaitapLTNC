6e9cf76909fb431d87708619ad608b12702da167
Ducwjbu <162545435+Ducwjbu@users.noreply.github.com>
2024-04-25 14:29:53 +0700

    Update 4.5.cpp

--- 4.5.cpp
+++ 4.5.cpp
@@ -3,43 +3,19 @@
 using namespace std;
 
 int main(){
-	int n; cin >> n;
-	int m; cin >> m;
-	int a[n];
-	int b[m];
-	
-	for(int i = 0;i<n;i++){
-		cin >> a[i];
-	}
-	
-	for(int i = 0;i<m;i++){
-		cin >> b[i];
-	}
-	
-	sort(a, a+n);
-	
-	int count = 0;
-	
-	sort(b, b+n+1);
-bool flag = true;	
-	for(int i = a[n-1]; i<= b[0];i++){
-		for(int x : a){
-			if(i%x != 0){
-				break;
-			}
-				for(int x : b){
-				if(x%i != 0){
-					flag = false;
-					break;
-				}
-			if(flag == true){
-				count++;
-				break;
-			}
-			}
-			flag = true;
-		}
-	}
-	
-	cout << count;
+    int n; cin >> n;
+    int a[n];
+    int b[n+1];
+    int sum1 = 0;
+    int sum2 = 0;
+    for(int i = 0;i<n;i++){
+        cin >> a[i];
+        sum1 += a[i];
+    }
+    for(int i = 0;i<n+1;i++){
+        cin >> b[i];
+        sum2 += b[i];
+    }
+    
+    cout << sum2 - sum1;
 }
